498680c0173e32851a02f665e80e05a43427211c
klausalves <klausmoroni@gmail.com>
2022-06-09 19:20:48 -0300

    commit

--- week_01/JS/firstProject.js
+++ week_01/JS/firstProject.js
@@ -1,4 +1,2 @@
 const node = document.getElementById('update-timestamp');
-node.innerHTML = new Date().toDateString() + '  ' + new Date().toLocaleTimeString() ;
-
-console.log(node);
+node.innerHTML = new Date(document.lastModified);
